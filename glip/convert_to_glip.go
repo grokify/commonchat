8e7f8b7c365855f341d06cf515810e27fe3594f9
John Wang <johncwang@gmail.com>
2017-02-18 23:31:54 -0800

    initial commit

---
+++ glip/convert_to_glip.go
@@ -0,0 +1,146 @@
+package commonchat
+
+import (
+	"fmt"
+	"strings"
+
+	cc "github.com/grokify/commonchat"
+	"github.com/grokify/glip-go-webhook"
+)
+
+type GlipMessageConverter struct {
+	EmojiURLFormat                 string
+	UseMarkdownQuote               bool
+	UseShortFields                 bool
+	ActivityIncludeIntegrationName bool
+}
+
+func NewGlipMessageConverter() GlipMessageConverter {
+	cv := GlipMessageConverter{}
+	return cv
+}
+
+func (cv *GlipMessageConverter) ConvertCommonMessage(commonMessage cc.Message) glipwebhook.GlipWebhookMessage {
+	glip := glipwebhook.GlipWebhookMessage{
+		Activity: commonMessage.Activity,
+		Title:    commonMessage.Title,
+		Icon:     commonMessage.IconURL}
+	if len(commonMessage.IconURL) > 0 {
+		glip.Icon = commonMessage.IconURL
+	} else if len(commonMessage.IconEmoji) > 0 {
+		iconURL, err := cc.EmojiToURL(cv.EmojiURLFormat, commonMessage.IconEmoji)
+		if err == nil {
+			glip.Icon = iconURL
+		}
+	}
+	bodyLines := []string{}
+	if len(commonMessage.Text) > 0 {
+		bodyLines = append(bodyLines, commonMessage.Text)
+	}
+	if len(commonMessage.Attachments) > 0 {
+		attachmentText := cv.RenderAttachments(commonMessage.Attachments)
+		if len(attachmentText) > 0 {
+			bodyLines = append(bodyLines, attachmentText)
+		}
+	}
+	if len(bodyLines) > 0 {
+		glip.Body = strings.Join(bodyLines, "\n")
+	}
+	return glip
+}
+
+func (cv *GlipMessageConverter) GetGlipMarkdownBodyPrefix() string {
+	if cv.UseMarkdownQuote {
+		return "> "
+	}
+	return ""
+}
+
+func (cv *GlipMessageConverter) RenderAttachments(attachments []cc.Attachment) string {
+	lines := []string{}
+	prefix := cv.GetGlipMarkdownBodyPrefix()
+	shortFields := []cc.Field{}
+	for _, att := range attachments {
+		if len(att.Title) > 0 {
+			lines = append(lines, fmt.Sprintf("%s**%s**", prefix, att.Title))
+		}
+		if len(att.Text) > 0 {
+			lines = append(lines, fmt.Sprintf("%s%s", prefix, att.Text))
+		}
+		for _, field := range att.Fields {
+			if !cv.UseShortFields {
+				field.Short = false
+			}
+			if field.Short {
+				shortFields = append(shortFields, field)
+				if len(shortFields) == 2 {
+					fieldLines := cv.BuildShortFieldLines(shortFields)
+					if len(fieldLines) > 0 {
+						lines = append(lines, fieldLines...)
+					}
+					shortFields = []cc.Field{}
+				}
+				continue
+			} else {
+				if len(shortFields) > 0 {
+					fieldLines := cv.BuildShortFieldLines(shortFields)
+					if len(fieldLines) > 0 {
+						lines = append(lines, fieldLines...)
+					}
+				}
+				shortFields = []cc.Field{}
+			}
+			if len(field.Title) > 0 {
+				lines = append(lines, fmt.Sprintf("%s**%s**", prefix, field.Title))
+			}
+			if len(field.Value) > 0 {
+				lines = append(lines, fmt.Sprintf("%s%s", prefix, field.Value))
+			}
+		}
+	}
+	return strings.Join(lines, "\n")
+}
+
+func (cv *GlipMessageConverter) BuildShortFieldLines(shortFields []cc.Field) []string {
+	lines := []string{}
+	prefix := cv.GetGlipMarkdownBodyPrefix()
+	for len(shortFields) > 0 {
+		if len(shortFields) >= 2 {
+			field1 := shortFields[0]
+			field2 := shortFields[1]
+			if len(field2.Title) > 0 || len(field2.Title) > 0 {
+				lines = append(lines, fmt.Sprintf("%s| **%v** | **%v** |", prefix, field1.Title, field2.Title))
+			}
+			if len(field2.Value) > 0 || len(field2.Value) > 0 {
+				lines = append(lines, fmt.Sprintf("%s| %v | %v |", prefix, field1.Value, field2.Value))
+			}
+			shortFields = shortFields[2:]
+		} else {
+			field1 := shortFields[0]
+			if len(field1.Title) > 0 {
+				lines = append(lines, fmt.Sprintf("%s**%s**", prefix, field1.Title))
+			}
+			if len(field1.Value) > 0 {
+				lines = append(lines, fmt.Sprintf("%s%s", prefix, field1.Value))
+			}
+			shortFields = shortFields[1:]
+		}
+	}
+	return lines
+}
+
+func (cv *GlipMessageConverter) RenderMessage(message cc.Message) string {
+	lines := []string{}
+	attachments := cv.RenderAttachments(message.Attachments)
+	if len(attachments) > 0 {
+		lines = append(lines, attachments)
+	}
+	return strings.Join(lines, "\n")
+}
+
+func (cv *GlipMessageConverter) IntegrationActivitySuffix(displayName string) string {
+	if cv.ActivityIncludeIntegrationName {
+		return fmt.Sprintf(" (%v)", displayName)
+	}
+	return ""
+}
