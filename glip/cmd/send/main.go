4ff6a0ce643a24b14102f50ad23c36a2eea95e1b
John Wang <johncwang@gmail.com>
2021-11-07 23:40:00 -0800

    feat: glip: add `./glip/cmd/send`

---
+++ glip/cmd/send/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/grokify/commonchat/examples"
+	"github.com/grokify/commonchat/glip"
+	"github.com/grokify/commonchat/glip/config"
+	glipwebhook "github.com/grokify/go-glip"
+	"github.com/grokify/simplego/fmt/fmtutil"
+	"github.com/jessevdk/go-flags"
+	"github.com/valyala/fasthttp"
+)
+
+type Options struct {
+	URL string `short:"u" long:"url" description:"url for webhook" required:"true"`
+}
+
+func main() {
+	opts := Options{}
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cfg := config.DefaultConverterConfig()
+	cfg.UseAttachments = true
+
+	ad, err := glip.NewGlipAdapter("", cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmtutil.PrintJSON(ad.CommonConverter.Config)
+
+	msi := map[string]interface{}{"useAttachments": false}
+
+	cfg2, err := ad.CommonConverter.Config.UpsertMSI(msi)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmtutil.PrintJSON(cfg2)
+
+	glMsg := &glipwebhook.GlipWebhookMessage{}
+
+	msg := examples.ExampleHookBodyAttachment()
+
+	req, res, err := ad.SendWebhook(opts.URL, msg, glMsg, map[string]interface{}{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fasthttp.ReleaseRequest(req)
+	fasthttp.ReleaseResponse(res)
+
+	if 1 == 1 {
+		req, res, err := ad.SendWebhook(opts.URL, msg, glMsg,
+			map[string]interface{}{"useAttachments": false})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fasthttp.ReleaseRequest(req)
+		fasthttp.ReleaseResponse(res)
+	}
+
+	fmt.Println("DONE")
+}
